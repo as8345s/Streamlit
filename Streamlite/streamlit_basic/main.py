4d18aec11781e735a353aa00e75970d157d0151d
as8345s <Alexander.Schechtel@alumni.fh-aachen.de>
2024-06-13 12:27:54 +0200

    Added TF Notebooks

--- Streamlite/streamlit_basic/main.py
+++ Streamlite/streamlit_basic/main.py
@@ -1,38 +1,69 @@
 """
-Autor:           Alexander Schechtel
-Description:     Basic usage of Streamlit. https://streamlit.io
+Autor:           Alex.S
+Description:     ---
 Written in:      IDE PyCharm.
-
-About this Code:
-The app will show basic usage of Streamlit
 """
 
 import streamlit as st
 import PIL
 import time
-import sklearn as sk
+
+
+def page_1() -> None:
+    st.title("Data Exploration")
+    st.write("This is the data exploration page.")
+
+
+def page_2() -> None:
+    st.title("Model Training")
+    st.write("This is the model training page.")
+
+
+def page_home() -> None:
+    """ Home page.
+    The first page you see when you start the application.
+    :return: None
+    """
+    st.title("Home Page")
+    st.write("Welcome to the home page!")
 
 
 def main() -> None:
-    st.title("Welcome user")
-    st.write("What is your name?")
-    user_input = st.text_input("My name is... ")
+    """
+    Start point of main programm.
+    :return: None
+    """
 
-    # Button
-    button_clicked = st.button("Click Me!")
-    # Handle button click
-    if button_clicked:
-        # Button click
-        st.write("Button clicked!")
-        if user_input:
-            st.write("You entered:", user_input)
-        else:
-            st.write("You didn't enter any text.")
+    # Directory for available pages.
+    pages = {
+        "Home": page_home,
+        "First page": page_1,
+        "Second page": page_2
+    }
 
-    st.write("# 😭")
+    # Trying out some of the Streamlit Widgets.
+    # Streamlit offers many types of Widgets like buttons and text boxes for user input.
+    # - Each widget has its own parameters
 
-    op = [1, 2]
-    st.checkbox("Agree")
+    def print_stuff():
+        st.write('Callable')
+
+    # Button:
+    if st.button(label="Button", key="unique_key", help="help info", on_click=print_stuff):
+        st.write('Button pressed')
+    else:
+        st.write('Button not pressed')
+
+    checkbox_val = False
+    if st.checkbox('Show and '):
+        checkbox_val=True
+    else:
+        checkbox_val = False
+
+    st.write(f'Check box value: {checkbox_val}')
+
+
+
 
 
 if __name__ == "__main__":
---
+++ Streamlite/streamlit_basic/pages/page_1.py
@@ -0,0 +1,10 @@
+"""
+Autor:           Alex.S
+Description:     ---
+Written in:      IDE PyCharm.
+"""
+
+import streamlit as st
+
+
+st.write("Page 1")
